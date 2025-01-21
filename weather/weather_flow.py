a2d6e399a8753f9bb30a4aaa12c568381d93cfdf
Peter <lilv447@gmail.com>
2025-01-21 03:03:09 -0500

    updated error handling

--- weather/weather_flow.py
+++ weather/weather_flow.py
@@ -52,7 +52,7 @@ class WeatherFlow:
 
             return response.json()
         except requests.exceptions.RequestException as e:
-            print(f'Error occured: {e}')
+            print(f'Request error occured: {e}')
             return None
 
     def parse_weather(self, weather: dict) -> tuple[str, str, float, float, str, str, float, float, float]:
@@ -60,31 +60,35 @@ class WeatherFlow:
         Parses weather dictionary from API and returns extracted data.
         Params: Expects json that is returned from API call made in get_weather.
         '''
-        location = weather.get('location', {})
-        current =  weather.get('current', {})
-        condition = current.get('condition', {})
+        # Checking if api actually returned data
+        if weather is None:
+            raise ValueError('Check that location is valid and try again')
+        else:
+            location = weather.get('location', {})
+            current =  weather.get('current', {})
+            condition = current.get('condition', {})
 
-        # Checks if location data is missing
-        if location is None or current is None or condition is None:
-            raise ValueError('Weather data from API was not returned, and cannot be parsed, as expected')
+            # Checks if location data is missing
+            if location is None or current is None or condition is None:
+                raise ValueError('Weather data from API was returned improperly, and cannot be parsed as expected')
 
-        # Retreiving items from dictionary returned from API and returning them as a tuple
-        name, region = location.get('name', 'Unknown'), location.get('region', 'Unknown')
-        country = location.get('country', 'Unknown')
-        text = condition.get('text', 'Unknown condition') 
-        icon = condition.get('icon', 'Icon unavailable')
-        
-        # Imperial data
-        temp_f = current.get('temp_f', 'Unknown temp' )
-        feelslike_f = current.get('feelslike_f', 'Unknown')
-        wind_mph = current.get('wind_mph', 'Wind speed unknown')
+            # Retreiving items from dictionary returned from API and returning them as a tuple
+            name, region = location.get('name', 'Unknown'), location.get('region', 'Unknown')
+            country = location.get('country', 'Unknown')
+            text = condition.get('text', 'Unknown condition') 
+            icon = condition.get('icon', 'Icon unavailable')
+            
+            # Imperial data
+            temp_f = current.get('temp_f', 'Unknown temp' )
+            feelslike_f = current.get('feelslike_f', 'Unknown')
+            wind_mph = current.get('wind_mph', 'Wind speed unknown')
 
-        # Metric data
-        temp_c = current.get('temp_c', 'Unknown temp')
-        feelslike_c =  current.get('feelslike_c', 'Unknown')
-        wind_kph = current.get('wind_kph', 'Wind speed unknown')
+            # Metric data
+            temp_c = current.get('temp_c', 'Unknown temp')
+            feelslike_c =  current.get('feelslike_c', 'Unknown')
+            wind_kph = current.get('wind_kph', 'Wind speed unknown')
 
-        return name, region, country, text, icon, temp_f, feelslike_f, wind_mph, temp_c, feelslike_c, wind_kph
+            return name, region, country, text, icon, temp_f, feelslike_f, wind_mph, temp_c, feelslike_c, wind_kph
     
     def parse_forecast(self, forcast: dict, metric: bool = False) -> None:
         '''
